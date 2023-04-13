fbec584cb81182ddd46ba6f5ba08b25a101efd49
kunal-gulati-dev <knlgulati365@gmail.com>
2023-04-13 12:08:39 +0530

    added the jobster project

---
+++ src/pages/Landing.js
@@ -0,0 +1,26 @@
+import logo from "../assets/images/logo.svg"
+import main from "../assets/images/main.svg"
+
+const Landing = () => {
+  return (
+    <main>
+        <nav>
+            <img src={logo} alt="jobster logo" className="logo" />
+        </nav>
+        <div className="container page" >
+            <div className="info">
+                <h1>
+                    job <span>tracking</span> app
+                </h1>
+                <p>
+                    Lorem, ipsum dolor sit amet consectetur adipisicing elit. Sint laborum illo ipsa officiis aliquam a animi harum repellat eius quidem.
+                </p>
+                <button className="btn btn-hero">Login/Register</button>
+            </div>
+            <img src={main} alt="job hunt" className="img main-img" />
+        </div>
+    </main>
+  )
+}
+
+export default Landing
